d5ff265a31f17ecfdf6ef1c22b7317bd968badae
Alexis Chacon <66844946+OnlyAlec@users.noreply.github.com>
2022-07-21 03:06:35 -0500

    perf: :zap: Cambio de estructura para mejor comprension

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -6,36 +6,43 @@ module.exports = {
       "color: white; background: purple;"
     );
     if (interaction.customId !== undefined) {
+      const command = interaction.customId.split(" : ")[0];
       console.log(
         `%c=>\tCUSTOM_ID: ${interaction.customId}\n`,
         "color: white; background: purple;"
       );
-    }
-    if (
-      interaction.customId
-        ? interaction.customId.includes("set_command")
-        : false
-    ) {
-      if (!interaction.isButton()) return;
-      BcK.commands
-        .find((command) => command.data.name === "set_command")
-        .execute(interaction, BcK);
-    } else if (
-      interaction.customId ? interaction.customId.includes("stats") : false
-    ) {
-      if (!interaction.isButton()) return;
-      await BcK.commands
-        .find((command) => command.data.name === "stats")
-        .execute(interaction, BcK);
-    } else if (
-      interaction.customId
-        ? interaction.customId.includes("embeds_playlists")
-        : false
-    ) {
-      if (!interaction.isButton()) return;
-      // TODO: Poner a playlist dentro del cliente, sin que sea un auto-command
-      const playlists = require("../commands/auto_commands/playlists.js");
-      await playlists.execute(interaction, BcK);
+      switch (command) {
+        case "set_command":
+          if (!interaction.isButton()) return;
+          BcK.commands
+            .find((command) => command.data.name === "set_command")
+            .execute(interaction, BcK);
+          break;
+
+        case "stats":
+          if (!interaction.isButton()) return;
+          BcK.commands
+            .find((command) => command.data.name === "stats")
+            .execute(interaction, BcK);
+          break;
+
+        case "embeds_playlists":
+          if (!interaction.isButton()) return;
+          // TODO: Poner a playlist dentro del cliente, sin que sea un auto-command
+          const playlists = require("../commands/auto_commands/playlists.js");
+          await playlists.execute(interaction, BcK);
+          break;
+
+        case "embeds_colors":
+          if (!interaction.isButton()) return;
+          const colores = require("../commands/Funciones-Embed/colores.js");
+          await colores.execute(interaction, BcK);
+          break;
+
+        default:
+          console.log("No se encuentra el comando!");
+          break;
+      }
     }
   },
 };
