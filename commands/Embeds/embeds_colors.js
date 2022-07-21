9fdc70a4d5b5766a3283d89d42062a0ce50aee4d
Alexis Chacon <66844946+OnlyAlec@users.noreply.github.com>
2022-07-21 03:07:29 -0500

    feat: :sparkles: Nuevo Embed y Funcion de Colores
    Con la reaccion a la interaccion asigna el rol al usuario

---
+++ commands/Embeds/embeds_colors.js
@@ -0,0 +1,140 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("embeds_colors")
+    .setDescription("Muestra los colores disponibles para los roles!"),
+  // name: "embeds_colores",
+  // description: "Muestra los colores disponibles para los roles",
+  async execute(interaction) {
+    console.log(
+      `%c❖ \t Se envio un embed colors `,
+      "background: #AD8BF0; color: white"
+    );
+    await interaction.deferReply();
+
+    const colores = new MessageEmbed()
+      .setColor("#7fb5b5")
+      .setTitle("˜”°•.˜”°• 𝒞𝑜𝓁𝑜𝓇𝑒𝓈 𝓅𝒶𝓇𝒶 𝓉𝑜𝒹𝑜𝓈 ! •°”˜.•°”˜")
+      .setAuthor({
+        name: "BeckS - Server",
+        iconURL: "https://i.imgur.com/9mMUVDh.png",
+        url: "https://github.com/OnlyAlec/Discord-BcK-Server",
+      })
+      .setDescription(
+        "*Claro que si, un poco de personalización a tu nombre no esta mal, aquí tienes lo colores que te puedes poner para que aparezca en tu nombre.* <a:blobDance:878353650815221781>"
+      )
+      .addFields(
+        {
+          name: "Rojo          🟥",
+          value: "\u200B",
+          inline: true,
+        },
+        {
+          name: "Naranja    🟧",
+          value: "\u200B",
+          inline: true,
+        },
+        {
+          name: "Amarillo   🟨",
+          value: "\u200B",
+          inline: true,
+        },
+        {
+          name: "Verde       🟩",
+          value: "\u200B",
+          inline: true,
+        },
+        {
+          name: "Cian          🔵",
+          value: "\u200B",
+          inline: true,
+        },
+        {
+          name: "Azul           🟦",
+          value: "\u200B",
+          inline: true,
+        },
+        {
+          name: "Morado   🟪",
+          value: "\u200B",
+          inline: true,
+        },
+        {
+          name: "Rosa         🌸",
+          value: "\u200B",
+          inline: true,
+        },
+        {
+          name: "Cafe           🟫",
+          value: "\u200B",
+          inline: true,
+        },
+        {
+          name: "Blanco      ⬜",
+          value: "\u200B",
+          inline: true,
+        }
+      )
+      .setFooter({
+        text: "Acuerdate necesitas darle click a uno de los botones de abajo! 👇🏻",
+        iconURL: "https://i.imgur.com/zGeu5zT.gif",
+      });
+    const select1 = new MessageActionRow().addComponents(
+      new MessageButton()
+        .setCustomId(`${this.data.name} : 🟥`)
+        .setLabel("")
+        .setStyle("SECONDARY")
+        .setEmoji("🟥"),
+      new MessageButton()
+        .setCustomId(`${this.data.name} : 🟧`)
+        .setLabel("")
+        .setStyle("SECONDARY")
+        .setEmoji("🟧"),
+      new MessageButton()
+        .setCustomId(`${this.data.name} : 🟨`)
+        .setLabel("")
+        .setStyle("SECONDARY")
+        .setEmoji("🟨"),
+      new MessageButton()
+        .setCustomId(`${this.data.name} : 🟩`)
+        .setLabel("")
+        .setStyle("SECONDARY")
+        .setEmoji("🟩"),
+      new MessageButton()
+        .setCustomId(`${this.data.name} : 🔵`)
+        .setLabel("")
+        .setStyle("SECONDARY")
+        .setEmoji("🔵")
+    );
+
+    const select2 = new MessageActionRow().addComponents(
+      new MessageButton()
+        .setCustomId(`${this.data.name} : 🟦`)
+        .setLabel("")
+        .setStyle("SECONDARY")
+        .setEmoji("🟦"),
+      new MessageButton()
+        .setCustomId(`${this.data.name} : 🌸`)
+        .setLabel("")
+        .setStyle("SECONDARY")
+        .setEmoji("🌸"),
+      new MessageButton()
+        .setCustomId(`${this.data.name} : 🟫`)
+        .setLabel("")
+        .setStyle("SECONDARY")
+        .setEmoji("🟫"),
+      new MessageButton()
+        .setCustomId(`${this.data.name} : ⬜`)
+        .setLabel("")
+        .setStyle("SECONDARY")
+        .setEmoji("⬜")
+    );
+
+    await interaction.editReply({
+      components: [select1, select2],
+      embeds: [colores],
+    });
+  },
+};
---
+++ commands/Funciones-Embed/colores.js
@@ -0,0 +1,211 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "roles_colores",
+  description: "Se asiga el color elgido como rol",
+  async execute(interaction) {
+    try {
+      await interaction.deferReply({ ephemeral: true });
+      console.log("%c⇢\t Colores ", "background: #CE796B; color: white");
+      let color_New, color_Old;
+      const color = interaction.customId.split(": ")[1];
+      const statusRol = check_Rol(interaction);
+      if (statusRol.exist == true)
+        color_Old = await remove_Rol(statusRol.role, interaction);
+      switch (color) {
+        case "🟥": {
+          color_New = await set_rol("Rojo", interaction);
+          break;
+        }
+        case "🟧": {
+          color_New = await set_rol("Naranja", interaction);
+          break;
+        }
+        case "🟨": {
+          color_New = await set_rol("Amarillo", interaction);
+          break;
+        }
+        case "🟩": {
+          color_New = await set_rol("Verde", interaction);
+          break;
+        }
+        case "🔵": {
+          color_New = await set_rol("Cian", interaction);
+          break;
+        }
+        case "🟦": {
+          color_New = await set_rol("Azul", interaction);
+          break;
+        }
+        case "🟪": {
+          color_New = await set_rol("Morado", interaction);
+          break;
+        }
+        case "🌸": {
+          color_New = await set_rol("Rosa", interaction);
+          break;
+        }
+        case "🟫": {
+          color_New = await set_rol("Cafe", interaction);
+          break;
+        }
+        case "⬜": {
+          color_New = await set_rol("Blanco", interaction);
+          break;
+        }
+
+        default:
+          console.log(
+            "%c⇢\t Error color no existente " + color,
+            "background: #CE796B; color: white"
+          );
+          break;
+      }
+      embeds(interaction, color_Old, color_New);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
+
+function check_Rol(interaccion) {
+  // Debo de checar si ya tiene un color puesto
+  // En caso de que si y sea el mismo color pues no hacer nada
+  // De lo contrario si es otro color, quitar el rol que tiene y poner el nuevo
+  const user = interaccion.member;
+  let band = false;
+  let role_Exist;
+  user.roles.cache.forEach((role) => {
+    if (role.name.includes("<") && band === false) {
+      band = true;
+      role_Exist = role;
+    }
+  });
+  const status = { exist: band, role: role_Exist };
+  return status;
+}
+
+async function remove_Rol(rol, interaction) {
+  let error = false;
+  const user = interaction.member;
+  await user.roles
+    .remove(rol)
+    .catch(error)
+    .then(() => {
+      if (!error) {
+        console.log(
+          "%c⇢\t Se quito rol: " + rol.name,
+          "background: #CE796B; color: white"
+        );
+      } else return error;
+    });
+  return rol.name;
+}
+
+async function set_rol(color, interaction) {
+  let error = false;
+  const user = interaction.member;
+  const roles = await interaction.guild.roles.fetch();
+  const user_Rol = roles.find(
+    (color_rol) => color_rol.name === "< " + color + " >"
+  );
+  await user.roles
+    .add(user_Rol)
+    .catch(error)
+    .then(() => {
+      if (!error) {
+        console.log(
+          "%c⇢\t Rol asignado: " + user_Rol.name,
+          "background: #CE796B; color: white"
+        );
+      } else return error;
+    });
+  return user_Rol.name;
+}
+
+async function embeds(interaction, color_Old, color_New) {
+  const emoji_Old = emoji(color_Old);
+  const emoji_New = interaction.customId.split(" : ")[1];
+  const correct = new MessageEmbed()
+    .setColor("RANDOM")
+    .setTitle("˜”°•.˜”°• 𝒞𝑜𝓁𝑜𝓇𝑒𝓈 𝓅𝒶𝓇𝒶 𝓉𝑜𝒹𝑜𝓈 ! •°”˜.•°”˜")
+    .setAuthor({
+      name: "BeckS - Server",
+      iconURL: "https://i.imgur.com/9mMUVDh.png",
+      url: "https://github.com/OnlyAlec/Discord-BcK-Server",
+    })
+    .addField("Tu nuevo color ahora es: ", `${color_New}   ${emoji_New}`, true);
+
+  const change = new MessageEmbed()
+    .setColor("RANDOM")
+    .setTitle("˜”°•.˜”°• 𝒞𝑜𝓁𝑜𝓇𝑒𝓈 𝓅𝒶𝓇𝒶 𝓉𝑜𝒹𝑜𝓈 ! •°”˜.•°”˜")
+    .setAuthor({
+      name: "BeckS - Server",
+      iconURL: "https://i.imgur.com/9mMUVDh.png",
+      url: "https://github.com/OnlyAlec/Discord-BcK-Server",
+    })
+    .addFields(
+      {
+        name: "Anterior Color               ->",
+        value: `${color_Old}   ${emoji_Old}`,
+        inline: true,
+      },
+      {
+        name: "Nuevo Color",
+        value: `${color_New}   ${emoji_New}`,
+        inline: true,
+      }
+    );
+
+  if (color_Old) {
+    await interaction.editReply({
+      embeds: [change],
+      ephemeral: true,
+    });
+  } else {
+    await interaction.editReply({
+      embeds: [correct],
+      ephemeral: true,
+    });
+  }
+}
+
+function emoji(color) {
+  if (!color) return;
+  color = color.split(" ")[1];
+  switch (color) {
+    case "Rojo":
+      return "🟥";
+      break;
+    case "Naranja":
+      return "🟧";
+      break;
+    case "Amarillo":
+      return "🟨";
+      break;
+    case "Verde":
+      return "🟩";
+      break;
+    case "Cian":
+      return "🔵";
+      break;
+    case "Azul":
+      return "🟦";
+      break;
+    case "Morado":
+      return "🟪";
+      break;
+    case "Rosa":
+      return "🌸";
+      break;
+    case "Cafe":
+      return "🟫";
+      break;
+    case "Blanco":
+      return "⬜";
+      break;
+    default:
+      return null;
+      break;
+  }
+}
